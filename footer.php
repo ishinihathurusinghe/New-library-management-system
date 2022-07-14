33b65deedfcb67d53ccd4fd8a3891d918ce38a51
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-07-14 11:17:07 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
                  {
                ?>
                  </main>
-                 <footer class="py-4 bg-light mt-auto">
+                  <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
                             <div class="text-muted">Copyright &copy; Library Management System <?php echo date('Y'); ?></div>
