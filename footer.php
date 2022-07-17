7d16642cc186b74f3fdd7f0428fba92f971d3d91
Ishini Hathurusinghe <101358580+ishinihathurusinghe@users.noreply.github.com>
2022-07-17 17:54:40 +0530

    Create footer.php

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 		       
                ?>
 
-                 </main>
+                  </main>
                   <footer class="py-4 bg-light mt-auto">
                     <div class="container-fluid px-4">
                         <div class="d-flex align-items-center justify-content-between small">
